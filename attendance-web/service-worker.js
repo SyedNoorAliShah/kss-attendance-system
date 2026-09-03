21f57abf826844262aeee343a6f0295de0a96d25
Syed Noor Ali Shah <noorashah61@gmail.com>
2026-09-03 11:59:31 +0500

    Add service worker for PWA installability

---
+++ attendance-web/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "kss-attendance-v1";
+
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
+});
